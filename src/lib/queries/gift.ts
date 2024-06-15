275cd9b1a15f5b4e20b1d9f8605457665735f7cd
Arnaud Gathy <arnaud.gathy@gmail.com>
2024-06-15 12:12:43 +0200

    Added error handling for queries

--- src/lib/queries/gift.ts
+++ src/lib/queries/gift.ts
@@ -7,53 +7,76 @@ import { unstable_noStore as noStore } from "next/cache";
 
 export const getMyGiftsAddedByMe = async () => {
   noStore();
-  const currentUser = await getCurrentUserEmail();
+  try {
+    const currentUser = await getCurrentUserEmail();
 
-  return prisma.gift.findMany({
-    where: { ownedBy: { email: currentUser }, addedBy: { email: currentUser } },
-    include: { ownedBy: true, addedBy: true, selectedBy: true },
-    orderBy: { name: "asc" },
-  });
+    return prisma.gift.findMany({
+      where: {
+        ownedBy: { email: currentUser },
+        addedBy: { email: currentUser },
+      },
+      include: { ownedBy: true, addedBy: true, selectedBy: true },
+      orderBy: { name: "asc" },
+    });
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch gifts.");
+  }
 };
 
 export const getOtherPeoplesGiftsAddedByMe = async () => {
   noStore();
-  const currentUser = await getCurrentUserEmail();
+  try {
+    const currentUser = await getCurrentUserEmail();
 
-  return prisma.gift.findMany({
-    where: {
-      ownedBy: { email: { not: currentUser } },
-      addedBy: { email: currentUser },
-    },
-    include: { ownedBy: true, addedBy: true, selectedBy: true },
-    orderBy: { name: "asc" },
-  });
+    return prisma.gift.findMany({
+      where: {
+        ownedBy: { email: { not: currentUser } },
+        addedBy: { email: currentUser },
+      },
+      include: { ownedBy: true, addedBy: true, selectedBy: true },
+      orderBy: { name: "asc" },
+    });
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch gifts.");
+  }
 };
 
 export const getMySelectedGifts = async () => {
   noStore();
-  const currentUser = await getCurrentUserEmail();
+  try {
+    const currentUser = await getCurrentUserEmail();
 
-  return prisma.gift.findMany({
-    where: { selectedBy: { email: currentUser } },
-    include: { ownedBy: true, addedBy: true, selectedBy: true },
-    orderBy: { name: "asc" },
-  });
+    return prisma.gift.findMany({
+      where: { selectedBy: { email: currentUser } },
+      include: { ownedBy: true, addedBy: true, selectedBy: true },
+      orderBy: { name: "asc" },
+    });
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch gifts.");
+  }
 };
 
 export const getGiftsByUsersExceptMine = async (unclaimedOnly: boolean) => {
   noStore();
-  const currentUser = await getCurrentUserEmail();
-  const selectByClause = unclaimedOnly ? { selectedBy: null } : {};
+  try {
+    const currentUser = await getCurrentUserEmail();
+    const selectByClause = unclaimedOnly ? { selectedBy: null } : {};
 
-  const gifts = await prisma.gift.findMany({
-    where: { ownedBy: { email: { not: currentUser } }, ...selectByClause },
-    include: { ownedBy: true, addedBy: true, selectedBy: true },
-    orderBy: [
-      { selectedById: { sort: "asc", nulls: "first" } },
-      { name: "asc" },
-    ],
-  });
+    const gifts = await prisma.gift.findMany({
+      where: { ownedBy: { email: { not: currentUser } }, ...selectByClause },
+      include: { ownedBy: true, addedBy: true, selectedBy: true },
+      orderBy: [
+        { selectedById: { sort: "asc", nulls: "first" } },
+        { name: "asc" },
+      ],
+    });
 
-  return groupBy(gifts, (gift) => gift.ownedBy.name);
+    return groupBy(gifts, (gift) => gift.ownedBy.name);
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch gifts.");
+  }
 };
